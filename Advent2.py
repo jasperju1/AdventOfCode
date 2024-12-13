17028d37613b4ea58362e1052490ba4f5f5fd0d3
jasperju <jasperjakob18@gmail.com>
2024-12-13 11:12:29 +0200

    completed2

--- Advent2.py
+++ Advent2.py
@@ -1,7 +1,31 @@
 with open('example2.txt', 'r') as f:
     lines = f.readlines()
-
+    counter = 0
     for line in lines:
-        print(line.split())
-        for x in range()
-            print(x)
+        num = line.split()
+        print(num)
+
+        is_safe = True
+        direction = ""
+        for i in range(len(num) - 1):
+            a = int(num[i])
+            b = int(num[i+1])
+            if i == 0 and a > b:
+                direction = "down"
+            elif i == 0 and a < b:
+                direction = "up"
+
+            pairdirection = ""
+            if a > b:
+                pairdirection = "down"
+            elif a < b:
+                pairdirection = "up"
+            
+            if not (abs(a-b) in [1,2,3] and pairdirection == direction and direction != ""):
+            
+                is_safe = False
+
+        print(is_safe)
+        if is_safe:
+            counter +=1
+    print(counter)
